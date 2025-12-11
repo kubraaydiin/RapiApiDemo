99a196160125ed4933e6fc9b95170b91834777cf
kubraaydiin <kubraaydiin96@gmail.com>
2025-12-11 17:04:50 +0300

    edited

---
+++ RapiApiDemo/Controllers/DutyPharmacyController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapiApiDemo.Models;
+
+namespace RapiApiDemo.Controllers
+{
+    public class DutyPharmacyController : Controller
+    {
+        public async Task<IActionResult> DutyPharmacy()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://nobetci-eczaneler-api-turkiye.p.rapidapi.com/pharmacy/today"),
+                Headers =
+    {
+        { "x-rapidapi-key", "7e2610f543msh4b5c8515e9e8191p122a55jsn5c750da7d0ed" },
+        { "x-rapidapi-host", "nobetci-eczaneler-api-turkiye.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                
+                var result = JsonConvert.DeserializeObject<DutyPharmacyViewModel>(body);
+                return View(result);
+            }
+        }
+    }
+}
---
+++ RapiApiDemo/Models/DutyPharmacyViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace RapiApiDemo.Models
+{
+    public class DutyPharmacyViewModel
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+        public DateTime recent_update { get; set; }
+        public List<Location> locations { get; set; }
+    }
+    public class Location
+    {
+        public string city { get; set; }
+        public string district { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+    }
+}
