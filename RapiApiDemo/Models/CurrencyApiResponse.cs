5fb94cd0799cc52f57792a6081dfda325f517767
kubraaydiin <kubraaydiin96@gmail.com>
2025-12-11 15:56:16 +0300

    edited

---
+++ RapiApiDemo/Models/CurrencyApiResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace RapiApiDemo.Models
+{
+    public class CurrencyApiResponse
+    {
+        public bool Success { get; set; }
+        public Dictionary<string, string> Symbols { get; set; }
+    }
+}
